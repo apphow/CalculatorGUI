01bbb877164fdf17be9055366fca8e3deba66882
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-05-25 00:39:51 -0400

    GUI.form, and created calculator buttons using swing

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel JavaCalculator;
+    private JTextField Display;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
+}
